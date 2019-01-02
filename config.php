209e61cd88bdac67dc61944abb3143e340aaa5ad
Sagar Deshmukh <sagarsdeshmukh91@gmail.com>
2019-01-02 12:00:07 +0530

    Initial commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "shareboard");
+
+// Define URL
+define("ROOT_PATH", "/shareboard/");
+define("ROOT_URL", "http://localhosttest/shareboard/");
